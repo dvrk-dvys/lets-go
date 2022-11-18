2918e115444db9d8e74188afb0f4759159052d2e
jordan harris <ja.harr91@gmail.com>
2022-11-18 19:48:56 +0100

    test git

---
+++ hello/hello.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+//https://www.digitalocean.com/community/tutorials/how-to-build-go-executables-for-multiple-platforms-on-ubuntu-20-04
